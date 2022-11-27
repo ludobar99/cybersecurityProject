f18151723a3fa45f8de62bd23882c94d8dbdfde9
Anvelicon <39614095+anvelicon@users.noreply.github.com>
2022-11-27 15:51:26 +0100

    refactor: deleted useless files

--- scripts/install.js
+++
@@ -1,9 +0,0 @@
-const { exec } = require("child_process");
-
-exec("mvn clean install package", (error) => {
-  if (error) {
-    console.error(`Error: ${error}`);
-  }
-
-  console.log("Install success!");
-});
--- scripts/reset.js
+++
@@ -1,9 +0,0 @@
-const { exec } = require("child_process");
-
-exec("mvn clean package", (error) => {
-  if (error) {
-    console.error(`Error: ${error}`);
-  }
-
-  console.log("Install success!");
-});
