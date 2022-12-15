2680702b9aae700e9bcaffd596332618bda4c38d
Anvelicon <39614095+anvelicon@users.noreply.github.com>
2022-12-15 20:54:42 +0100

    refactor: updated setup scripts

--- generateEnv.bat
+++ generateEnv.bat
@@ -1,10 +1,9 @@
 @echo off
 
-SET varPathToFile=\Applications\java-2022-03\Eclipse.app\Contents\MacOS\.env
-echo "creating .env file in ... $varPathToFile"
-touch "$varPathToFile"
-echo "DB_USER="sa""  > $varPathToFile
-echo "SA_PASSWORD="Strong.Pwd-123""  >> $varPathToFile
-echo "DRIVER_CLASS="com.microsoft.sqlserver.jdbc.SQLServerDriver"" >> $varPathToFile
-echo "DB_URL="jdbc:sqlserver://localhost:1433;databaseName=examDB;encrypt=true;trustServerCertificate=true;"" >> $varPathToFile
-echo ".env file created."
+IF "-e" ".env" (
+  echo "Found .env file in current directory"
+  COPY  ".env" "%~1\.env"
+  echo "Successfully copied .env file to destination"
+) ELSE (
+  echo "Missing .env file in current directory!"
+)
--- generateEnv.sh
+++ generateEnv.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
-
-varPathToFile=/Users/marconms/Downloads/apache-tomcat-10.0.27/bin/.env
-echo "creating .env file..."
-echo $varPathToFile
-
-touch $varPathToFile
-
-echo "DB_USER=\"sa\"" > $varPathToFile
-echo "SA_PASSWORD=\"Strong.Pwd-123\"" >> $varPathToFile
-echo "DRIVER_CLASS=\"com.microsoft.sqlserver.jdbc.SQLServerDriver\"" >> $varPathToFile
-echo "DB_URL=\"jdbc:sqlserver://localhost:1433;databaseName=examDB;encrypt=true;trustServerCertificate=true;\"" >> $varPathToFile
-
-echo ".env file created."
+if [ -e .env ]
+then
+    echo "Found .env file in current directory"
+    cp .env $1/.env
+    echo "Successfully copied .env file to destination"
+else
+    echo "Missing .env file in current directory!"
+fi
