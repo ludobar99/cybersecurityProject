79ad119ed8195559e92433bc23345c2430adbd1e
ludobar99 <ludobar99@gmail.com>
2022-12-17 18:35:23 +0100

    removed User generate keys method

--- src/main/java/client/User.java
+++ src/main/java/client/User.java
@@ -1,10 +1,5 @@
 package client;
 
-import asymmetricEncryption.KeysGenerator;
-
-import java.io.IOException;
-import java.security.NoSuchAlgorithmException;
-import java.security.NoSuchProviderException;
 import java.security.PublicKey;
 
 public class User {
@@ -19,18 +14,6 @@ public class User {
 		this.email = email;
 		this.password = pwd;
 	}
-
-	/*
-	 * Writes privateKey on a file and returns user's publicKey
-	 */
-	public PublicKey createKeys(String filePath) throws NoSuchAlgorithmException, NoSuchProviderException, IOException {
-		KeysGenerator keyGen = new KeysGenerator(1024);
-		keyGen.createKeys();
-		keyGen.writePrivateKeyToFile(filePath, keyGen.getPrivateKey().getEncoded());
-
-		return keyGen.getPublicKey();
-	}
-
 
 	public String getEmail() {
 		return email;
