52f7aad4190eb0963634ecc8280b8d040dbc9af2
Anvelicon <39614095+anvelicon@users.noreply.github.com>
2022-12-18 21:34:56 +0100

    chore: minor changes

--- generateEnv.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-IF "-e" ".env" (
-  echo "Found .env file in current directory"
-  COPY  ".env" "%~1\.env"
-  echo "Successfully copied .env file to destination"
-) ELSE (
-  echo "Missing .env file in current directory!"
-)
